925c6b4a9b68c155f081199b648c942b1d0d9fd1
GNS-USA <gns.usak@gmail.com>
2017-08-09 05:37:56 +0900

    Add android platform, face camera and gallery
    - 안드로이드 플랫폼 추가 및 구동
    - 카메라 및 갤러리 기능 활성화

--- src/pages/register-facephoto/register-facephoto.ts
+++ src/pages/register-facephoto/register-facephoto.ts
@@ -1,10 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { RegisterCarInfoPage } from '../register-car-info/register-car-info';
 import { RegisterDriverPrefPage } from '../register-driver-pref/register-driver-pref';
 
 import { FacePhoto, RegisterType } from '../../form/formData.model';
 import { FormDataService } from '../../form/formData.service';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 /**
  * Generated class for the RegisterFacephotoPage page.
@@ -17,6 +18,9 @@ import { FormDataService } from '../../form/formData.service';
 @Component({
   selector: 'page-register-facephoto',
   templateUrl: 'register-facephoto.html',
+  providers: [
+    Camera
+  ]
 })
 export class RegisterFacephotoPage {
   registerType: RegisterType;
@@ -25,7 +29,7 @@ export class RegisterFacephotoPage {
   form: any;
   valid:boolean = false;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, private formDataService: FormDataService) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, private formDataService: FormDataService, private camera:Camera) {
   }
 
   ngOnInit() {
@@ -36,16 +40,42 @@ export class RegisterFacephotoPage {
   ionViewDidLoad() {
   }
 
-  takephoto() {
-    this.facePhoto.facePhotoLocation = '../../assets/1.jpg';
-    this.valid = true;
-    this.photoSelected = true;
+  takephoto(type:string) {
+    let options: CameraOptions = {
+      quality: 100,
+      destinationType: this.camera.DestinationType.DATA_URL,
+      encodingType: this.camera.EncodingType.JPEG,
+      mediaType: this.camera.MediaType.PICTURE,
+      targetWidth : 1024,
+      targetHeight : 1024,
+      correctOrientation: true
+    }
+    if(type === 'camera') {
+      this.camera.getPicture(options).then((imageData) => {
+        this.facePhoto.facePhotoLocation = 'data:image/jpeg;base64,' + imageData;
+        this.valid = true;
+        this.photoSelected = true;
+      }, (err) => {
+        console.log("photo error");
+      })
+    }else if(type === 'gallery'){
+      options.sourceType = this.camera.PictureSourceType.PHOTOLIBRARY;
+
+      this.camera.getPicture(options).then((imageData) => {
+        this.facePhoto.facePhotoLocation = 'data:image/jpeg;base64,' + imageData;
+        this.valid = true;
+        this.photoSelected = true;
+      }, (err) => {
+        console.log("photo error");
+      })
+    }
+
   }
-  
+
   goto() {
     this.formDataService.setFacePhoto(this.facePhoto);
     this.formDataService.setType(this.registerType);
-    
+
     if (this.registerType.userType == 'rider') {
       this.navCtrl.push(RegisterCarInfoPage);
     }
